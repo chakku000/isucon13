9930b0d21e5626cd33505c7bcc581a22db8ec652
45deg <45deg@users.noreply.github.com>
2023-11-25 12:02:24 +0900

    n+1を直す

--- webapp/go/livestream_handler.go
+++ webapp/go/livestream_handler.go
@@ -483,45 +483,42 @@ func getLivecommentReportsHandler(c echo.Context) error {
 
 	return c.JSON(http.StatusOK, reports)
 }
-
 func fillLivestreamResponse(ctx context.Context, tx *sqlx.Tx, livestreamModel LivestreamModel) (Livestream, error) {
-	ownerModel := UserModel{}
-	if err := tx.GetContext(ctx, &ownerModel, "SELECT * FROM users WHERE id = ?", livestreamModel.UserID); err != nil {
-		return Livestream{}, err
-	}
-	owner, err := fillUserResponse(ctx, tx, ownerModel)
-	if err != nil {
-		return Livestream{}, err
-	}
+    ownerModel := UserModel{}
+    if err := tx.GetContext(ctx, &ownerModel, "SELECT * FROM users WHERE id = ?", livestreamModel.UserID); err != nil {
+        return Livestream{}, err
+    }
+    owner, err := fillUserResponse(ctx, tx, ownerModel)
+    if err != nil {
+        return Livestream{}, err
+    }
 
-	var livestreamTagModels []*LivestreamTagModel
-	if err := tx.SelectContext(ctx, &livestreamTagModels, "SELECT * FROM livestream_tags WHERE livestream_id = ?", livestreamModel.ID); err != nil {
-		return Livestream{}, err
-	}
+    var tagModels []TagModel
+    query := `SELECT t.* FROM tags t
+              JOIN livestream_tags lt ON t.id = lt.tag_id
+              WHERE lt.livestream_id = ?`
+    if err := tx.SelectContext(ctx, &tagModels, query, livestreamModel.ID); err != nil {
+        return Livestream{}, err
+    }
 
-	tags := make([]Tag, len(livestreamTagModels))
-	for i := range livestreamTagModels {
-		tagModel := TagModel{}
-		if err := tx.GetContext(ctx, &tagModel, "SELECT * FROM tags WHERE id = ?", livestreamTagModels[i].TagID); err != nil {
-			return Livestream{}, err
-		}
+    tags := make([]Tag, len(tagModels))
+    for i, tagModel := range tagModels {
+        tags[i] = Tag{
+            ID:   tagModel.ID,
+            Name: tagModel.Name,
+        }
+    }
 
-		tags[i] = Tag{
-			ID:   tagModel.ID,
-			Name: tagModel.Name,
-		}
-	}
-
-	livestream := Livestream{
-		ID:           livestreamModel.ID,
-		Owner:        owner,
-		Title:        livestreamModel.Title,
-		Tags:         tags,
-		Description:  livestreamModel.Description,
-		PlaylistUrl:  livestreamModel.PlaylistUrl,
-		ThumbnailUrl: livestreamModel.ThumbnailUrl,
-		StartAt:      livestreamModel.StartAt,
-		EndAt:        livestreamModel.EndAt,
-	}
-	return livestream, nil
+    livestream := Livestream{
+        ID:           livestreamModel.ID,
+        Owner:        owner,
+        Title:        livestreamModel.Title,
+        Tags:         tags,
+        Description:  livestreamModel.Description,
+        PlaylistUrl:  livestreamModel.PlaylistUrl,
+        ThumbnailUrl: livestreamModel.ThumbnailUrl,
+        StartAt:      livestreamModel.StartAt,
+        EndAt:        livestreamModel.EndAt,
+    }
+    return livestream, nil
 }
